89e066b4e6ba1f366a3ab275522ae40ae51c5bbf
Gaston Javier Ricchiardi <192486168+gastonjavierricchiardi@users.noreply.github.com>
2026-08-03 19:04:07 -0300

    Correcciones mínimas de 01EJ01

--- ejercicios/01EJ01/01EJ01_1.ts
+++ ejercicios/01EJ01/01EJ01_1.ts
@@ -1,8 +1,8 @@
 /**
-1.​Crear una clase Persona que tenga los atributos públicos nombre y apellido.
-●​ Crear una instancia y asignarle valores.
-●​ Mostrar por pantalla los valores asignados.
- */
+1.​ Crear una clase Persona que tenga los atributos públicos nombre y apellido.
+* Crear una instancia y asignarle valores.
+* Mostrar por pantalla los valores asignados.
+*/
 export {};
 
 class Persona {
